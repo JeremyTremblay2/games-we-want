ad8292c52b7c416121b4d826c49dc2a72778193f
Jérémy Tremblay <jeremy-tremblay@outlook.fr>
2024-04-18 14:58:23 +0200

    [#4] Add image management

--- src/hooks/useGameGeneralData.js
+++ src/hooks/useGameGeneralData.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
-import { API_TOP_10_GAMES_URL } from '../utils/constants'
+import { API_TOP_10_GAMES_URL, API_SCREENSHOTS_URL, API_BASE_URL } from '../utils/constants'
+import game_image from '../assets/images/game_image_placeholder.png'
 import moment from 'moment'
 
 const useWeather = () => {
@@ -9,7 +10,7 @@ const useWeather = () => {
 		async function getData() {
 			// API call
 			const result = await fetch(
-				API_TOP_10_GAMES_URL
+				`${API_BASE_URL}${API_TOP_10_GAMES_URL}?pageSize=10&page=1`
 			)
 
 			if (!result.ok) {
@@ -17,13 +18,24 @@ const useWeather = () => {
 			}
 
 			const data = await result.json()
-			console.log(data)
 
 			// Formattage
 			const gamesList = [];
 			for (let i = 0; i < data.length; i++) {
+				let image
+				const screenshot = await fetch(
+					`${API_BASE_URL}${API_SCREENSHOTS_URL}/${data[i].id}`
+				)
+				console.log(screenshot)
+				console.log(`${API_BASE_URL}${API_SCREENSHOTS_URL}/${data[i].id}`)
+				if (screenshot.ok && screenshot.status === 200) {
+					image = (await screenshot.json())[0].url
+				}
+				if (!image) {
+					image = game_image
+				}
 				gamesList.push({
-					image: 'https://cdn.akamai.steamstatic.com/steam/apps/413150/capsule_616x353.jpg',
+					image: image,
                     firstReleaseDate: moment(data[i].firstReleaseDate).format('L'),
                     name: data[i].name
 				})
