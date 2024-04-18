b775a86404af2dc09d5110862ab153650271fbf4
Maxime Croisier <crmaxime@gmail.com>
2024-04-18 11:13:53 +0200

    Add Material UI Theme Provider

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createTheme, CssBaseline, ThemeProvider, useMediaQuery } from "@mui/material"
+import { useMemo } from "react"
 import Home from "../Home/index.jsx"
 
 const router = createBrowserRouter([
@@ -6,14 +8,28 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <Home/>
+        element: <Home />
       }
     ]
   }
 ])
 
 export default function Router() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light'
+        }
+      }),
+    [prefersDarkMode]
+  )
+
   return (
-    <RouterProvider router={router}/>
+    <ThemeProvider theme={theme} >
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider >
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom/client'
 import Router from './components/Router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router/>
-  </React.StrictMode>
+  <Router/>
 )
