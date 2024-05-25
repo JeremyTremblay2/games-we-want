59f0438aa4cd7c3106f00acf546d6f323654d816
Maxime Croisier <crmaxime@gmail.com>
2024-05-25 19:39:54 +0200

    [#11 #12 #20] Persist User Data & Automatic Redirect when not connected

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { createTheme, CssBaseline, styled, ThemeProvider, useMediaQuery } from "@mui/material"
+import { createTheme, CssBaseline, ThemeProvider, useMediaQuery } from "@mui/material"
 import { useMemo } from "react"
+import Layout from "../Layout/index.jsx"
+import NotificationsProvider from "../Notifications/index.jsx"
 import Home from "../Home/index.jsx"
 import GameDetails from "../GameDetails/index.jsx"
 import Login from "../Login/index.jsx"
-import Layout from "../Layout/index.jsx"
-import NotificationsProvider from "../Notifications/index.jsx"
+import PrivateRoute from "./PrivateRoute.jsx"
+import { UserContextProvider } from "../UserContext/index.jsx"
 
 const router = createBrowserRouter([
   {
@@ -17,7 +19,11 @@ const router = createBrowserRouter([
       },
       {
         path: "/game/:gameId",
-        element: <GameDetails/>
+        element: (
+          <PrivateRoute>
+            <GameDetails />
+          </PrivateRoute>
+        )
       },
       {
         path: "/login",
@@ -40,8 +46,8 @@ export default function Router() {
           mode: prefersDarkMode ? 'dark' : 'light'
         },
         shape: {
-          borderRadius: 15,
-        },
+          borderRadius: 15
+        }
       }),
     [prefersDarkMode]
   )
@@ -50,7 +56,9 @@ export default function Router() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <NotificationsProvider prefersDarkMode={prefersDarkMode} />
-      <RouterProvider router={router} />
+      <UserContextProvider>
+        <RouterProvider router={router} />
+      </UserContextProvider>
     </ThemeProvider>
   )
 }
