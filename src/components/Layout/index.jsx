ab12a60506c398203925b8d6e7abbc7a5988a2c3
Jérémy Tremblay <jeremy-tremblay@outlook.fr>
2024-06-12 17:58:55 +0200

    [#9] Add visual search bar

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,6 +1,6 @@
-import { useContext, useEffect, useState } from 'react'
-import { Link, Outlet, useNavigate } from 'react-router-dom'
-import Menu from '@mui/material/Menu'
+import { useContext, useEffect, useState } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import Menu from "@mui/material/Menu";
 import {
   AppBar,
   Toolbar,
@@ -8,99 +8,136 @@ import {
   Container,
   Typography,
   MenuItem,
-  IconButton
-} from "@mui/material"
-import AccountCircle from '@mui/icons-material/AccountCircle'
-import { handleDisconnect } from "../../hooks/useAuthenticate"
-import { UserContext, useUserInfo } from "../UserContext/index.jsx"
-import { useIsLoading } from "../LoadingContext/index.jsx"
-import ScrollTop from "./ScrollTop"
-import LinearProgress from "@mui/material/LinearProgress"
+  IconButton,
+} from "@mui/material";
+import Box from "@mui/material/Box";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import { handleDisconnect } from "../../hooks/useAuthenticate";
+import { UserContext, useUserInfo } from "../UserContext/index.jsx";
+import { useIsLoading } from "../LoadingContext/index.jsx";
+import ScrollTop from "./ScrollTop";
+import LinearProgress from "@mui/material/LinearProgress";
+import { Margin } from "@mui/icons-material";
 
 const Layout = () => {
-  const userInfo = useUserInfo()
-  const [anchorEl, setAnchorEl] = useState(null)
+  const userInfo = useUserInfo();
+  const [anchorEl, setAnchorEl] = useState(null);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const userContext = useContext(UserContext)
-  const { isLoading } = useIsLoading()
+  const userContext = useContext(UserContext);
+  const { isLoading } = useIsLoading();
 
   useEffect(() => {
     if (userInfo === null) {
-      userContext.setRefreshUser(true)
+      userContext.setRefreshUser(true);
     }
-  }, [userInfo])
-
+  }, [userInfo]);
 
   const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget)
-  }
+    setAnchorEl(event.currentTarget);
+  };
 
   const handleClickProfile = () => {
-    navigate("/profile")
-    setAnchorEl(null)
-  }
+    navigate("/profile");
+    setAnchorEl(null);
+  };
 
   const handleClickDisconnect = () => {
-    setAnchorEl(null)
-    handleDisconnect(navigate)
-    userContext.setRefreshUser(true)
-  }
+    setAnchorEl(null);
+    handleDisconnect(navigate);
+    userContext.setRefreshUser(true);
+  };
 
   return (
     <>
-      <AppBar position="static" id="top-anchor">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link to="/" style={{ textDecoration: "none", color: "unset" }}>
-              Games we Want
-            </Link>
-          </Typography>
-          {userInfo ? (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
+      <Box>
+        <AppBar position="static" id="top-anchor">
+          <Toolbar sx={{ alignItems: "center" }}>
+            <Typography variant="h6" component="div">
+              <Link to="/" style={{ textDecoration: "none", color: "unset" }}>
+                Games we Want
+              </Link>
+            </Typography>
+            {userInfo ? (
+              <div
+                style={{ display: "flex", alignItems: "center", flexGrow: 1 }}
               >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'right'
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right'
-                }}
-                open={Boolean(anchorEl)}
-                onClose={() => setAnchorEl(null)}
-              >
-                <MenuItem onClick={handleClickProfile}>Profile</MenuItem>
-                <MenuItem onClick={handleClickDisconnect}>Disconnect</MenuItem>
-              </Menu>
-            </div>) : (
-            <Button color="inherit" onClick={() => navigate("/login")}>Login</Button>
-          )}
-        </Toolbar>
-      </AppBar>
-      {isLoading && (
-        <LinearProgress style={{ width: "100%" }} />
-      )}
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    borderRadius: "50px",
+                    backgroundColor: "rgba(255, 255, 255, 0.15)",
+                    "&:hover": {
+                      backgroundColor: "rgba(255, 255, 255, 0.25)",
+                    },
+                    padding: "0 10px",
+                    margin: "auto",
+                    flexGrow: 0.3,
+                  }}
+                >
+                  <SearchIcon sx={{ fontSize: 22 }} />
+                  <InputBase
+                    placeholder="Search…"
+                    inputProps={{ "aria-label": "search" }}
+                    sx={{
+                      color: "inherit",
+                      paddingLeft: "6px",
+                      flexGrow: 1,
+                    }}
+                  />
+                </Box>
+                <div style={{ alignItems: "end" }}>
+                  <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                    color="inherit"
+                  >
+                    <AccountCircle />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "right",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    open={Boolean(anchorEl)}
+                    onClose={() => setAnchorEl(null)}
+                  >
+                    <MenuItem onClick={handleClickProfile}>Profile</MenuItem>
+                    <MenuItem onClick={handleClickDisconnect}>
+                      Disconnect
+                    </MenuItem>
+                  </Menu>
+                </div>
+              </div>
+            ) : (
+              <Button color="inherit" onClick={() => navigate("/login")}>
+                Login
+              </Button>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Box>
+      {isLoading && <LinearProgress style={{ width: "100%" }} />}
       <Container maxWidth="xl" sx={{ paddingTop: "20px" }}>
         <Outlet />
       </Container>
       <ScrollTop />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
