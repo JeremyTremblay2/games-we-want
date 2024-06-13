ee4466c315aec50e80c0911cc506e91d008a5117
Maxime Croisier <crmaxime@gmail.com>
2024-06-13 16:27:54 +0200

    [#60] Fix eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,39 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
+
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
+  globals: {
+    vi: true
+  },
   rules: {
-    'no-unused-vars' : 'off',
+    'no-unused-vars': 'off',
     'react/no-unescaped-entities': 'off',
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true }
     ],
-  },
+    'react/jsx-first-prop-new-line': [2, 'multiline'],
+    'react/jsx-max-props-per-line': [
+      2,
+      { maximum: 1, when: 'multiline' }
+    ],
+    'react/jsx-indent-props': [2, 2],
+    'react/jsx-closing-bracket-location': [
+      2,
+      'tag-aligned'
+    ]
+  }
 }
