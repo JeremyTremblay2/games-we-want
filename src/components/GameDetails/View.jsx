31a412f899dd18b2c615c0e44dd0eca28e7a938d
Jérémy Tremblay <jeremy-tremblay@outlook.fr>
2024-06-13 14:16:34 +0200

    [#52] Fix display rounded corners

--- src/components/GameDetails/View.jsx
+++ src/components/GameDetails/View.jsx
@@ -8,26 +8,48 @@ import {
   AccordionDetails,
   AccordionSummary,
   Button,
+  Grid,
   Typography,
 } from "@mui/material"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 import OpenInNewIcon from "@mui/icons-material/OpenInNew"
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 const View = ({ game }) => {
+  const [expanded, setExpanded] = useState(game.description.split("\n").length <= 5)
   return (
     <>
       {game && (
         <>
           <h1>{game.name}</h1>
-          <p>{game.description}</p>
+          {game.description && (
+            <Accordion
+              sx={{ borderRadius: 0.35, "&:before": { display: "none" }, marginTop: 0.2 }}
+              expanded={expanded}
+              onChange={(event, isExpanded) => setExpanded(isExpanded)}
+            >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="game-description-content"
+                id="game-description-header"
+              >
+                <Typography>Description</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>{game.description}</Typography>
+              </AccordionDetails>
+            </Accordion>
+          )}
           <>
             {game.screenshots && game.screenshots.length > 1 ? (
-              <CenterSlider>
-                {game.screenshots?.map((screenshot, index) => (
-                  <img key={index} src={screenshot} alt={`Screenshot ${index + 1}`} />
-                ))}
-              </CenterSlider>
+              <Grid item xs={12} md={6}>
+                <CenterSlider>
+                  {game.screenshots?.map((screenshot, index) => (
+                    <img key={index} src={screenshot} alt={`Screenshot ${index + 1}`} />
+                  ))}
+                </CenterSlider>
+              </Grid>
             ) : (
               game.screenshots.length === 1 && (
                 <img src={game.screenshots?.[0]} alt={`Screenshot 1`} style={{ width: "100%" }} />
@@ -37,11 +59,14 @@ const View = ({ game }) => {
               <h2 style={{ fontSize: "24px" }}>Companies who developed this game</h2>
             )}
             {game.companies?.map((company, index) => (
-              <Accordion key={index}>
+              <Accordion
+                key={index}
+                sx={{ borderRadius: 0.35, "&:before": { display: "none" }, marginTop: 0.2 }}
+              >
                 <AccordionSummary
                   expandIcon={<ExpandMoreIcon />}
-                  aria-controls={`panel${index}-content`}
-                  id={`panel${index}-header`}
+                  aria-controls={`panel${index + 1}-content`}
+                  id={`panel${index + 1}-header`}
                   style={{ fontSize: "20px" }}
                 >
                   {company.name}
